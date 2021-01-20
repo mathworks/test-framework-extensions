06225fa87e414c91fb1af7807c762f4f406b80ae
Tim Johns <timjohns@mathworks.com>
2021-01-20 08:25:18 +0000

    Add support for ignoring NaT; allow string diagnostic to be created for multiple failures.

--- tbx/+matlab/+unittest/+constraints/DatetimeTolerance.m
+++ tbx/+matlab/+unittest/+constraints/DatetimeTolerance.m
@@ -1,38 +1,57 @@
 classdef DatetimeTolerance < matlab.unittest.constraints.Tolerance
     
     properties
-        Tolerance (1,1) duration = seconds(1)
+        Tolerance (1,1) duration = seconds(eps)
+        IgnoreNaT (1,1) logical = false
     end
     
     methods
         
-        function this = DatetimeTolerance(value)
+        function this = DatetimeTolerance(value,nvpairs)
+            
+            arguments
+                value (1,1) duration = seconds(eps)
+                nvpairs.IgnoringNaT (1,1) logical = false
+            end
             
             this.Tolerance = value;
+            this.IgnoreNaT = nvpairs.IgnoringNaT;
             
         end
 
         function tf = supports(~,value)
             
-            tf = isa(value, 'datetime');
+            tf = isa(value,"datetime") || isa(value,"duration");
             
         end
 
         function tf = satisfiedBy(this,actual,expected)
             
-            tf = abs(actual - expected) <= this.Tolerance;
+            withinTol = abs(actual - expected) <= this.Tolerance;
+            
+            if this.IgnoreNaT
+                ignore = isnat(actual) & isnat(expected);
+                withinTol(ignore) = true;
+            end
+            
+            tf = all(withinTol);
         
         end
 
         function diag = getDiagnosticFor(this,actual,expected)
             
+            actualDifference = string(actual-expected);
+            actualDifference(ismissing(actualDifference)) = "NaT";
+            actualDifference = strjoin(actualDifference,",");
+            
             str = "The datetimes have a difference of " + ...
-                string(actual-expected) + ". The allowable tolerance is " + ...
+                actualDifference + ". The allowable tolerance is " + ...
                 string(this.Tolerance);
                 
             diag = matlab.unittest.diagnostics.StringDiagnostic(str);
             
         end
+        
     end
     
 end
